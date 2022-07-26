5420fdf44edc903d319cb5c52a8e1ff01650d1d3
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-07-26 09:18:02 -0300

    Implementação Interface de repositório base

---
+++ TapiFramework/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TapiFramework.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(T entity);
+        Task<T> UpdateAsync(T entity); 
+        Task<T> RemoveAsync(T entity);
+        Task<IEnumerable<T>> RemoveRangeAsync(T entity);    
+    }
+}
