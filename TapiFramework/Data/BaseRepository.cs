ade73cf3eeebfa7b14f4b194b3de46a96095194d
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-07-26 09:54:27 -0300

    Criação BaseRepository e implementação GetAllAsync

---
+++ TapiFramework/Data/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TapiFramework.Interfaces;
+
+namespace TapiFramework.Data
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly DbContext context; 
+        public BaseRepository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
