8e7619adc29a2705740042c8696ff5d66f64f389
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-01 16:45:37 -0300

    Inserção estrutura ControllerBase

---
+++ TapiFramework/Controllers/ControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TapiFramework.Entities.Interfaces;
+using TapiFramework.Services.Interfaces;
+
+namespace TapiFramework.Controllers;
+
+public abstract class ControllerBase<TEntity, TBaseService> : ControllerBase
+    where TEntity : class, IBaseEntity
+    where TBaseService : class, IBaseServices<TEntity>
+{
+}
