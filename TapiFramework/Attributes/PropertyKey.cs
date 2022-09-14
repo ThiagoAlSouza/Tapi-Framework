70e31fd2281532fba6bba2d4699e2cd0b2e4304f
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-13 23:24:32 -0300

    Implementação propriedade NameColunm no atributo customizado

--- TapiFramework/Attributes/PropertyKey.cs
+++ TapiFramework/Attributes/PropertyKey.cs
@@ -1,7 +1,20 @@
 ﻿namespace TapiFramework.Attributes;
 
-[AttributeUsage(validOn:AttributeTargets.Property, AllowMultiple = false)]
+[AttributeUsage(validOn:AttributeTargets.Property)]
 public class PropertyKey : Attribute
 {
+    #region Constructor
 
+    public PropertyKey(string nameColunm)
+    {
+        NameColunm = nameColunm;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public string NameColunm { get; set; }
+
+    #endregion
 }
